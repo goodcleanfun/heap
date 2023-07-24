26f783db3bfedc8c8eeb98d949ec27da5d0a8ef9
Al <albarrentine@gmail.com>
2023-07-24 02:21:53 -0400

    [double] adding double_minheap and double_maxheap

---
+++ src/double_maxheap.h
@@ -0,0 +1,9 @@
+#ifndef DOUBLE_MAXHEAP_H
+#define DOUBLE_MAXHEAP_H
+
+#include "heap.h"
+#include "num_arrays/double_array.h"
+
+MAXHEAP_INIT(double_maxheap, double, double_array)
+
+#endif
---
+++ src/double_minheap.h
@@ -0,0 +1,9 @@
+#ifndef DOUBLE_MINHEAP_H
+#define DOUBLE_MINHEAP_H
+
+#include "heap.h"
+#include "num_arrays/double_array.h"
+
+MINHEAP_INIT(double_minheap, double, double_array)
+
+#endif
