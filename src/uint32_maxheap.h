6e624800f52bdf58388c41a20af6eace11417de2
Al <albarrentine@gmail.com>
2023-07-24 02:24:55 -0400

    [uint32] adding uint32 minheap/maxheap

---
+++ src/uint32_maxheap.h
@@ -0,0 +1,9 @@
+#ifndef UINT32_MAXHEAP_H
+#define UINT32_MAXHEAP_H
+
+#include "heap.h"
+#include "num_arrays/uint32_array.h"
+
+MAXHEAP_INIT(uint32_maxheap, uint32_t, uint32_array)
+
+#endif
---
+++ src/uint32_minheap.h
@@ -0,0 +1,9 @@
+#ifndef UINT32_MINHEAP_H
+#define UINT32_MINHEAP_H
+
+#include "heap.h"
+#include "num_arrays/uint32_array.h"
+
+MINHEAP_INIT(uint32_minheap, uint32_t, uint32_array)
+
+#endif
