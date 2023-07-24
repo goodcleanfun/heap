0a99ff5e147c08c3d451b9b482b78e9b3d72a44b
Al <albarrentine@gmail.com>
2023-07-24 02:26:20 -0400

    [uint64] adding uint64 minheap/maxheap

---
+++ src/uint64_maxheap.h
@@ -0,0 +1,9 @@
+#ifndef UINT64_MAXHEAP_H
+#define UINT64_MAXHEAP_H
+
+#include "heap.h"
+#include "num_arrays/uint64_array.h"
+
+MAXHEAP_INIT(uint64_maxheap, uint64_t, uint64_array)
+
+#endif
---
+++ src/uint64_minheap.h
@@ -0,0 +1,9 @@
+#ifndef UINT64_MINHEAP_H
+#define UINT64_MINHEAP_H
+
+#include "heap.h"
+#include "num_arrays/uint64_array.h"
+
+MINHEAP_INIT(uint64_minheap, uint64_t, uint64_array)
+
+#endif
