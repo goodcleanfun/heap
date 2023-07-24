d2a8be7ef05eef5cf91b8cc78c1c0e466c01edc3
Al <albarrentine@gmail.com>
2023-07-24 02:23:23 -0400

    [float] adding float minheap/maxheap

---
+++ src/float_maxheap.h
@@ -0,0 +1,9 @@
+#ifndef FLOAT_MAXHEAP_H
+#define FLOAT_MAXHEAP_H
+
+#include "heap.h"
+#include "num_arrays/float_array.h"
+
+MAXHEAP_INIT(float_maxheap, float, float_array)
+
+#endif
---
+++ src/float_minheap.h
@@ -0,0 +1,9 @@
+#ifndef FLOAT_MINHEAP_H
+#define FLOAT_MINHEAP_H
+
+#include "heap.h"
+#include "num_arrays/float_array.h"
+
+MINHEAP_INIT(float_minheap, float, float_array)
+
+#endif
